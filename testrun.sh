d62ae3b39d79f41ff03cb7a09e2457b4dfec4768
Franklin Wise <franklin@krave.io>
2015-05-18 16:32:10 -0700

    update

--- testrun.sh
+++ testrun.sh
@@ -1,7 +1,8 @@
- #!/bin/bash
- COUNTER=0
- while [  $COUNTER -lt 10 ]; do
-    echo The counter is $COUNTER
-    let COUNTER=COUNTER+1
-    sleep 2s
- done
+#!/bin/bash
+
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
+   echo The counter is $COUNTER
+   let COUNTER=COUNTER+1
+   sleep 2s
+done
